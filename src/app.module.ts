7fb0bf12dec6ea475fa7bf9a57859dbb1279d7c4
CM Scofield <scoundreldays2@gmail.com>
2025-03-02 13:26:07 -0300

    Removendo app: service/controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [PrismaModule, UserModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
